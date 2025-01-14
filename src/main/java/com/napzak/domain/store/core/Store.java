24d352ad85444656b49c87eb39307a1a97580dd8
sae2say <lollol0617@gmail.com>
2025-01-14 22:16:29 +0900

    [#9] feat: store 도메인 추가 구성

---
+++ src/main/java/com/napzak/domain/store/core/Store.java
@@ -0,0 +1,60 @@
+package com.napzak.domain.store.core;
+
+import com.napzak.domain.store.core.enums.Role;
+import com.napzak.domain.store.core.enums.SocialType;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class Store {
+    private final Long id;
+    private final String nickname;
+    private final String phoneNumber;
+    private final String photo;
+    private final Role role;
+    private final String description;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime deletedAt;
+    private final Long socialId;
+    private final SocialType socialType;
+
+    public Store(
+            Long id,
+            String nickname,
+            String phoneNumber,
+            String photo,
+            Role role,
+            String description,
+            LocalDateTime createdAt,
+            LocalDateTime deletedAt,
+            Long socialId,
+            SocialType socialType
+    ) {
+        this.id = id;
+        this.nickname = nickname;
+        this.phoneNumber = phoneNumber;
+        this.photo = photo;
+        this.role = role;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.deletedAt = deletedAt;
+        this.socialId = socialId;
+        this.socialType = socialType;
+    }
+
+    public static Store fromEntity(StoreEntity storeEntity) {
+        return new Store(
+                storeEntity.getId(),
+                storeEntity.getNickname(),
+                storeEntity.getPhoneNumber(),
+                storeEntity.getPhoto(),
+                storeEntity.getRole(),
+                storeEntity.getDescription(),
+                storeEntity.getCreatedAt(),
+                storeEntity.getDeletedAt(),
+                storeEntity.getSocialId(),
+                storeEntity.getSocialType()
+        );
+    }
+}
--- src/main/java/com/napzak/domain/store/core/StoreTableConstants.java
+++ src/main/java/com/napzak/domain/store/core/StoreTableConstants.java
@@ -4,7 +4,8 @@ public class StoreTableConstants {
 	public final static String TABLE_STORE = "store";
 	public final static String COLUMN_ID = "id";
 	public final static String COLUMN_NICKNAME = "nickname";
-	public final static String COLUMN_PHONE = "phone";
+	public final static String COLUMN_PHONE_NUMBER = "phone_number";
+	public final static String COLUMN_DESCRIPTION = "description";
 	public final static String COLUMN_PHOTO = "photo";
 	public final static String COLUMN_ROLE = "role";
 	public final static String COLUMN_CREATED_AT = "created_at";
