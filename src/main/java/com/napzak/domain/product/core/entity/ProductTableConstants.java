712e0257acbc489b970b5f4c70eadfc5e0e2ac72
sae2say <lollol0617@gmail.com>
2025-01-16 07:11:20 +0900

    [#48] fix(ProductTableConstants): 오타 수정

--- src/main/java/com/napzak/domain/product/core/entity/ProductTableConstants.java
+++ src/main/java/com/napzak/domain/product/core/entity/ProductTableConstants.java
@@ -13,8 +13,8 @@ public class ProductTableConstants {
     public final static String COLUMN_TRADE_STATUS = "trade_status";
     public final static String COLUMN_IS_PRICE_NEGOTIABLE = "is_price_negotiable";
     public final static String COLUMN_IS_DELIVERY_INCLUDED = "is_delivery_included";
-    public final static String COLUMN_STANDARD_DELIVERY_FEE = "standard_delivery_type";
-    public final static String COLUMN_HALF_DELIVERY_FEE = "half_delivery_free";
+    public final static String COLUMN_STANDARD_DELIVERY_FEE = "standard_delivery_fee";
+    public final static String COLUMN_HALF_DELIVERY_FEE = "half_delivery_fee";
     public final static String COLUMN_PRODUCT_CONDITION = "product_condition";
     public final static String COLUMN_STORE_ID = "store_id";
     public final static String COLUMN_GENRE_ID = "genre_id";
