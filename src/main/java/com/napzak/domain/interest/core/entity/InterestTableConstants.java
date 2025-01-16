3ce8befcaf74ee790c6634061dc42511cb49bb19
sae2say <lollol0617@gmail.com>
2025-01-17 01:32:35 +0900

    [#48] fix: interest table의 id명 오타 수정 review_id -> interest_id

--- src/main/java/com/napzak/domain/interest/core/entity/InterestTableConstants.java
+++ src/main/java/com/napzak/domain/interest/core/entity/InterestTableConstants.java
@@ -2,7 +2,7 @@ package com.napzak.domain.interest.core.entity;
 
 public class InterestTableConstants {
     public static final String TABLE_INTEREST = "interest";
-    public static final String COLUMN_ID = "review_id";
+    public static final String COLUMN_ID = "interest_id";
     public static final String COLUMN_STORE_ID = "store_id";
     public static final String COLUMN_PRODUCT_ID = "product_id";
 }
