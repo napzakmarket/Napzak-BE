1240bded5e719bd7ac1fc758d7cc65cea4ef8fdc
sae2say <lollol0617@gmail.com>
2025-01-16 18:04:36 +0900

    [#48] feat: banner 정보 조회 성공 success code 추가

--- src/main/java/com/napzak/domain/product/api/exception/ProductSuccessCode.java
+++ src/main/java/com/napzak/domain/product/api/exception/ProductSuccessCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ProductSuccessCode implements BaseSuccessCode {
-    PRODUCT_GET_SUCCESS(HttpStatus.OK, "상품 정보 불러오기 성공")
+    PRODUCT_GET_SUCCESS(HttpStatus.OK, "상품 정보 불러오기 성공"),
+    BANNER_GET_SUCCESS(HttpStatus.OK, "배너 정보 불러오기 성공")
     ;
 
     private final HttpStatus httpStatus;
