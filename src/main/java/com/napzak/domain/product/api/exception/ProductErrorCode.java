ad192fc4c17e6f636212870945fb8ee2d1f40dfc
sae2say <lollol0617@gmail.com>
2025-01-17 01:33:24 +0900

    [#48] feat: productEntity를 찾는 jpa 추가

--- src/main/java/com/napzak/domain/product/api/exception/ProductErrorCode.java
+++ src/main/java/com/napzak/domain/product/api/exception/ProductErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ProductErrorCode implements BaseErrorCode {
-    PRODUCT_IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "대표 이미지가 없습니다.");
+    PRODUCT_IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "대표 이미지가 없습니다."),
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 상품을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/com/napzak/domain/product/core/ProductRepository.java
+++ src/main/java/com/napzak/domain/product/core/ProductRepository.java
@@ -9,9 +9,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    Optional<ProductEntity> findById(Long productId);
 
     @Query(
             value = """
