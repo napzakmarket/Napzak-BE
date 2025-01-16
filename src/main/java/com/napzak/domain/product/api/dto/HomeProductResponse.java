9b16a32be651e2024b9e79a54c6fd9691aba6df6
sae2say <lollol0617@gmail.com>
2025-01-16 18:05:21 +0900

    [#48] refactor: service에서 파라미터를 직접 받아오도록 변경

--- src/main/java/com/napzak/domain/product/api/dto/HomeProductResponse.java
+++ src/main/java/com/napzak/domain/product/api/dto/HomeProductResponse.java
@@ -1,5 +1,6 @@
 package com.napzak.domain.product.api.dto;
 
+import com.napzak.domain.product.core.entity.enums.TradeType;
 import com.napzak.domain.product.core.vo.Product;
 
 public record HomeProductResponse(
@@ -16,18 +17,17 @@ public record HomeProductResponse(
 ) {
 
     public static HomeProductResponse of(
-            Product product,
+            Long productId,
+            String genreName,
+            String productName,
             String photo,
+            int price,
             String uploadTime,
-            Boolean isInterested){
+            Boolean isInterested,
+            String tradeType,
+            String tradeStatus,
+            Boolean isPriceNegotiable){
 
-        final Long productId = product.getId();
-        final String genreName = product.getGenre().getName();
-        final String productName = product.getTitle();
-        final int price = product.getPrice();
-        final String tradeType = product.getTradeType().toString();
-        final String tradeStatus = product.getTradeStatus().toString();
-        final Boolean isPriceNegotiable = product.getIsPriceNegotiable();
 
         return new HomeProductResponse(
                 productId,
