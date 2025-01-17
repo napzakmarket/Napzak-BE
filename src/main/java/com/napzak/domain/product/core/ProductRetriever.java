d5a92317c928f1744ec9edb85a7fc3c9c44a6029
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-01-18 01:30:02 +0900

    [#49] feat(ProductRetriever): ProductEntity를 조회 후 Product vo로 변환하는 ProductRetriever 추가

---
+++ src/main/java/com/napzak/domain/product/core/ProductRetriever.java
@@ -0,0 +1,55 @@
+package com.napzak.domain.product.core;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.napzak.domain.product.api.exception.ProductErrorCode;
+import com.napzak.domain.product.api.service.enums.SortOption;
+import com.napzak.domain.product.core.entity.ProductEntity;
+import com.napzak.domain.product.core.entity.enums.TradeType;
+import com.napzak.domain.product.core.vo.Product;
+import com.napzak.global.common.exception.NapzakException;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ProductRetriever {
+
+	private final ProductRepository productRepository;
+
+	@Transactional(readOnly = true)
+	public Product findById(Long id) {
+		ProductEntity productEntity = productRepository.findById(id)
+			.orElseThrow(() -> new NapzakException(ProductErrorCode.PRODUCT_NOT_FOUND));
+		return Product.fromEntity(productEntity);
+	}
+
+	@Transactional(readOnly = true)
+	public List<Product> retrieveProducts(
+		SortOption sortOption, Long cursorProductId, Integer cursorOptionalValue, int size,
+		Boolean isOnSale, Boolean isUnopened, List<Long> genreIds, TradeType tradeType) {
+
+		return productRepository.findProductsBySortOptionAndFilters(
+				sortOption.toOrderSpecifier(), cursorProductId, cursorOptionalValue, size,
+				isOnSale, isUnopened, genreIds, tradeType
+			).stream()
+			.map(Product::fromEntity)
+			.toList();
+	}
+
+	@Transactional(readOnly = true)
+	public List<Product> searchProducts(
+		String searchWord, SortOption sortOption, Long cursorProductId, Integer cursorOptionalValue, int size,
+		Boolean isOnSale, Boolean isUnopened, List<Long> genreIds, TradeType tradeType) {
+
+		return productRepository.searchProductsBySearchWordAndSortOptionAndFilters(
+				searchWord, sortOption.toOrderSpecifier(), cursorProductId, cursorOptionalValue, size,
+				isOnSale, isUnopened, genreIds, tradeType
+			).stream()
+			.map(Product::fromEntity)
+			.toList();
+	}
+}
