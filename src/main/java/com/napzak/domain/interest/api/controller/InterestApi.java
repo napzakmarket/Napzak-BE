044139cb37378e3aaa403085468e3187bc798410
sae2say <lollol0617@gmail.com>
2025-01-17 19:19:20 +0900

    [#57] refactor: requestParam -> pathVariable으로 받도록 변경

--- src/main/java/com/napzak/domain/interest/api/controller/InterestApi.java
+++ src/main/java/com/napzak/domain/interest/api/controller/InterestApi.java
@@ -3,12 +3,12 @@ package com.napzak.domain.interest.api.controller;
 import com.napzak.global.auth.annotation.CurrentMember;
 import com.napzak.global.common.exception.dto.SuccessResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 public interface InterestApi {
 
     ResponseEntity<SuccessResponse<Void>> postInterest(
-            @RequestParam Long productId,
+            @PathVariable("productId") Long productId,
             @CurrentMember final Long storeId
     );
 }
