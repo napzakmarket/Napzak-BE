e22ca47c308783977e88e6bf2796866ba4434be1
sae2say <lollol0617@gmail.com>
2025-01-17 19:13:26 +0900

    [#62] refactor: requestParam -> pathVariable으로 받도록 변경

--- src/main/java/com/napzak/domain/interest/api/controller/InterestApi.java
+++ src/main/java/com/napzak/domain/interest/api/controller/InterestApi.java
@@ -3,12 +3,18 @@ package com.napzak.domain.interest.api.controller;
 import com.napzak.global.auth.annotation.CurrentMember;
 import com.napzak.global.common.exception.dto.SuccessResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface InterestApi {
 
     ResponseEntity<SuccessResponse<Void>> postInterest(
-            @RequestParam Long productId,
+            @PathVariable("productId") Long productId,
+            @CurrentMember final Long storeId
+    );
+
+    ResponseEntity<SuccessResponse<Void>> deleteInterest(
+            @PathVariable("productId") Long productId,
             @CurrentMember final Long storeId
     );
 }
--- src/main/java/com/napzak/domain/interest/api/controller/InterestController.java
+++ src/main/java/com/napzak/domain/interest/api/controller/InterestController.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,9 +29,9 @@ public class InterestController {
     private final InterestRetriever interestRetriever;
     private final ProductRetriever productRetriever;
 
-    @PostMapping("/")
+    @PostMapping("/{productId}")
     public ResponseEntity<SuccessResponse<Void>> postInterest(
-            @RequestParam Long productId,
+            @PathVariable("productId") Long productId,
             @CurrentMember final Long storeId
     ) {
 
@@ -48,9 +49,9 @@ public class InterestController {
                 .body(SuccessResponse.of(InterestSuccessCode.POST_INTEREST_SUCCESS));
     }
 
-    @DeleteMapping("/")
+    @DeleteMapping("/{productId}")
     public ResponseEntity<SuccessResponse<Void>> deleteInterest(
-            @RequestParam Long productId,
+            @PathVariable("productId") Long productId,
             @CurrentMember final Long storeId
     ) {
 
