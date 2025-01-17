1993f1659fe9657a41df25230f5ddb4872c59018
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-01-18 01:32:44 +0900

    [#49] feat: interest 정보 가져오는 repository, retriever 추가

---
+++ src/main/java/com/napzak/domain/interest/core/InterestRepository.java
@@ -0,0 +1,36 @@
+package com.napzak.domain.interest.core;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.napzak.domain.interest.core.entity.InterestEntity;
+
+@Repository
+public interface InterestRepository extends JpaRepository<InterestEntity, Long> {
+	@Query("SELECT COUNT(i) > 0 " +
+		"FROM InterestEntity i " +
+		"WHERE i.productId = :productId AND i.storeId = :storeId")
+	boolean existsByProductIdAndStoreId(@Param("productId") Long productId,
+		@Param("storeId") Long storeId);
+
+	@Query("SELECT i.productId " +
+		"FROM InterestEntity i " +
+		"WHERE i.productId IN :productIds AND i.productId = :storeId")
+	List<Long> findLikedProductIdsByStore(@Param("productIds") List<Long> productIds,
+		@Param("storeId") Long storeId);
+
+	@Modifying
+	@Query("""
+                DELETE FROM InterestEntity i
+                WHERE i.storeId = :storeId
+                AND i.productId = :productId
+            """)
+	void deleteByProductIdAndStoreId(
+		@Param("productId") Long productId,
+		@Param("storeId") Long storeId);
+}
