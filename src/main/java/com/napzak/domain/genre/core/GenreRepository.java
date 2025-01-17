c627c4d152643239be825f499f730c1c4bceb636
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-01-18 01:33:04 +0900

    [#49] feat: Genre 정보 가져오는 repository, retriever 추가

---
+++ src/main/java/com/napzak/domain/genre/core/GenreRepository.java
@@ -0,0 +1,19 @@
+package com.napzak.domain.genre.core;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import com.napzak.domain.genre.core.entity.GenreEntity;
+
+@Repository
+public interface GenreRepository extends JpaRepository<GenreEntity, Long> {
+	@Query("SELECT g.name FROM GenreEntity g WHERE g.id = :id")
+	String findNameById(@Param("id") Long id);
+
+	@Query("SELECT g.id, g.name FROM GenreEntity g WHERE g.id IN :ids")
+	List<Object[]> findNamesByIds(@Param("ids") List<Long> ids);
+}
