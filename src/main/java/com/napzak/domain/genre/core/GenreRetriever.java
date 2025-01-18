8c07d67f685b3f774d7805fff0f56eebb63a5e9a
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-01-18 10:43:31 +0900

    [#70] feat(GenreRetriever): Genre 조회 관련 로직 추가 및 기존 로직 rename

--- src/main/java/com/napzak/domain/genre/core/GenreRetriever.java
+++ src/main/java/com/napzak/domain/genre/core/GenreRetriever.java
@@ -7,6 +7,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.napzak.domain.genre.core.vo.Genre;
+import com.napzak.domain.genre.api.service.enums.SortOption;
+
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -15,16 +18,36 @@ public class GenreRetriever {
 	private final GenreRepository genreRepository;
 
 	@Transactional(readOnly = true)
-	public String getGenreNameById(Long genreId) {
+	public String retrieveGenreNameById(Long genreId) {
 		return genreRepository.findNameById(genreId);
 	}
 
 	@Transactional(readOnly = true)
-	public Map<Long, String> getGenreNamesByIds(List<Long> genreIds) {
+	public Map<Long, String> retrieveGenreNamesByIds(List<Long> genreIds) {
 		return genreRepository.findNamesByIds(genreIds).stream()
 			.collect(Collectors.toMap(
 				result -> (Long)result[0],   // Key: Genre ID
 				result -> (String)result[1] // Value: Genre Name
 			));
+	}
+
+	@Transactional(readOnly = true)
+	public List<Genre> retrieveGenres(SortOption sortOption, Long cursorGenreId, int size) {
+
+		return genreRepository.getGenresByWithCursor(
+				sortOption.toOrderSpecifier(), cursorGenreId, size
+			).stream()
+			.map(Genre::fromEntity)
+			.toList();
+	}
+
+	@Transactional(readOnly = true)
+	public List<Genre> searchGenres(String searchWord, SortOption sortOption, Long cursorGenreId, int size) {
+
+		return genreRepository.searchGenresBySearchWordWithCursor(
+				searchWord, sortOption.toOrderSpecifier(), cursorGenreId, size
+			).stream()
+			.map(Genre::fromEntity)
+			.toList();
 	}
 }
