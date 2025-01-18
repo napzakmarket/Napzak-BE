78097a7af1d4242d876c7eae5d0b9dd6f5e0840b
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-01-18 10:42:09 +0900

    [#70] feat(GenrePagination): GenrePagination 추가

---
+++ src/main/java/com/napzak/domain/genre/api/service/GenrePagination.java
@@ -0,0 +1,48 @@
+package com.napzak.domain.genre.api.service;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import com.napzak.domain.genre.api.dto.request.cursor.OldestCursor;
+import com.napzak.domain.genre.api.exception.GenreErrorCode;
+import com.napzak.domain.genre.core.vo.Genre;
+import com.napzak.domain.genre.core.vo.GenreList;
+import com.napzak.global.common.exception.NapzakException;
+import com.napzak.global.common.exception.code.ErrorCode;
+import com.napzak.domain.genre.api.service.enums.SortOption;
+
+public class GenrePagination {
+	private final int needSize;
+	private final GenreList genreList;
+
+	public GenrePagination(int needSize, GenreList genreList) {
+		this.needSize = needSize;
+		if (genreList == null || genreList.isEmpty()) {
+			throw new NapzakException(GenreErrorCode.GENRE_NOT_FOUND);
+		}
+		this.genreList = genreList;
+	}
+
+	private boolean hasMoreData() {
+		return genreList.size() > needSize;
+	}
+
+	@Nullable
+	public String generateNextCursor(SortOption sortOption) {
+		if (!hasMoreData()) {
+			return null;
+		}
+		Genre lastGenre = genreList.getGenreList().get(needSize);
+		switch (sortOption) {
+			case OLDEST:
+				return new OldestCursor(lastGenre.getId()).toString();
+			default:
+				throw new NapzakException(ErrorCode.INVALID_SORT_OPTION);
+		}
+	}
+
+	public List<Genre> getGenreList() {
+		return genreList.getGenreList().subList(0, Math.min(needSize, genreList.size()));
+	}
+}
