d39634c1dc178bbbdf8ed940cb8ca01065936ea9
sae2say <lollol0617@gmail.com>
2025-01-20 05:47:56 +0900

    [#78] feat: interestCount를 증가, 감소시키는 쿼리작성

--- src/main/java/com/napzak/domain/product/core/ProductRepository.java
+++ src/main/java/com/napzak/domain/product/core/ProductRepository.java
@@ -1,11 +1,28 @@
 package com.napzak.domain.product.core;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.napzak.domain.product.core.entity.ProductEntity;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long>, ProductRepositoryCustom {
+    @Modifying
+    @Query("""
+            UPDATE ProductEntity p
+            SET p.interestCount = p.interestCount + 1 
+            WHERE p.id = :productId
+            """)
+    void incrementInterestCount(@Param("productId") Long productId);
 
+    @Modifying
+    @Query("""
+            UPDATE ProductEntity p
+            SET p.interestCount = p.interestCount - 1
+            WHERE p.id = :productId
+            AND p.interestCount > 0""")
+    void decrementInterestCount(@Param("productId") Long productId);
 }
