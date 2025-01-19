a6e6607f1ec46268c503410ba041e27bc2e47d2b
sae2say <lollol0617@gmail.com>
2025-01-20 06:00:02 +0900

    [#78] feat: controller, service 구현

---
+++ src/main/java/com/napzak/domain/interest/api/controller/InterestController.java
@@ -0,0 +1,73 @@
+package com.napzak.domain.interest.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.napzak.domain.interest.api.exception.InterestErrorCode;
+import com.napzak.domain.interest.api.exception.InterestSuccessCode;
+import com.napzak.domain.interest.api.service.InterestService;
+import com.napzak.domain.interest.core.InterestRetriever;
+import com.napzak.domain.product.api.exception.ProductErrorCode;
+import com.napzak.domain.product.core.ProductRetriever;
+import com.napzak.global.auth.annotation.CurrentMember;
+import com.napzak.global.common.exception.NapzakException;
+import com.napzak.global.common.exception.dto.SuccessResponse;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("api/v1/interest")
+public class InterestController {
+
+	private final InterestService interestService;
+	private final InterestRetriever interestRetriever;
+	private final ProductRetriever productRetriever;
+
+	@PostMapping("/{productId}")
+	public ResponseEntity<SuccessResponse<Void>> postInterest(
+		@PathVariable("productId") Long productId,
+		@CurrentMember final Long storeId
+	) {
+
+		if (!productRetriever.existsById(productId)) { //존재하지 않는 product
+			throw new NapzakException(ProductErrorCode.PRODUCT_NOT_FOUND);
+		}
+
+		if (interestRetriever.getIsInterested(productId, storeId)) {//이미 좋아요가 눌러져 있음
+			throw new NapzakException(InterestErrorCode.INTEREST_ALREADY_POSTED);
+		}
+
+		interestService.postInterest(productId, storeId);
+
+		return ResponseEntity.ok()
+			.body(SuccessResponse.of(InterestSuccessCode.POST_INTEREST_SUCCESS));
+	}
+
+	@DeleteMapping("/{productId}")
+	public ResponseEntity<SuccessResponse<Void>> deleteInterest(
+		@PathVariable("productId") Long productId,
+		@CurrentMember final Long storeId
+	) {
+
+		if (!productRetriever.existsById(productId)) { //존재하지 않는 product
+			throw new NapzakException(ProductErrorCode.PRODUCT_NOT_FOUND);
+		}
+
+		if (!interestRetriever.getIsInterested(productId, storeId)) { //좋아요가 눌려져 있지 않은 상태임
+			throw new NapzakException(InterestErrorCode.INTEREST_NOT_FOUND);
+		}
+
+		interestService.deleteInterest(productId, storeId);
+
+		return ResponseEntity.ok()
+			.body(SuccessResponse.of(InterestSuccessCode.DELETE_INTEREST_SUCCESS));
+	}
+
+}
---
+++ src/main/java/com/napzak/domain/interest/api/service/InterestService.java
@@ -0,0 +1,25 @@
+package com.napzak.domain.interest.api.service;
+
+import org.springframework.stereotype.Service;
+
+import com.napzak.domain.interest.core.InterestRemover;
+import com.napzak.domain.interest.core.InterestSaver;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class InterestService {
+
+	private final InterestSaver interestSaver;
+	private final InterestRemover interestRemover;
+
+	public void postInterest(Long productId, Long storeId) {
+		interestSaver.saveInterest(productId, storeId);
+	}
+
+	public void deleteInterest(Long productId, Long storeId) {
+		interestRemover.deleteInterest(productId, storeId);
+	}
+
+}
