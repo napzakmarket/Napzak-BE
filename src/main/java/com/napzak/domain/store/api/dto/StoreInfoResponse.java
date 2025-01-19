45fdc6014e67e809686d5baa7be83becd88f1e13
sae2say <lollol0617@gmail.com>
2025-01-20 05:12:16 +0900

    [#74] feat: response dto 작성

---
+++ src/main/java/com/napzak/domain/store/api/dto/StoreInfoResponse.java
@@ -0,0 +1,21 @@
+package com.napzak.domain.store.api.dto;
+
+import java.util.List;
+
+public record StoreInfoResponse(
+        Long storeId,
+        String storeNickName,
+        String storeDescription,
+        String storePhoto,
+        List<GenrePreferenceResponse> genrePreferences
+) {
+    public static StoreInfoResponse of(
+            final Long storeId,
+            final String storeNickName,
+            final String storeDescription,
+            final String storePhoto,
+            final List<GenrePreferenceResponse> genrePreferenceList
+    ){
+        return new StoreInfoResponse(storeId, storeNickName, storeDescription, storePhoto, genrePreferenceList);
+    }
+}
