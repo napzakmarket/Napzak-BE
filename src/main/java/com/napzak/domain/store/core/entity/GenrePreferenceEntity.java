246f75d4b8016933c729d4dd82600d08d0fd6055
sae2say <lollol0617@gmail.com>
2025-01-21 02:47:42 +0900

    [#88] feat(GenrePReferenceEntity): create 메서드 추가

--- src/main/java/com/napzak/domain/store/core/entity/GenrePreferenceEntity.java
+++ src/main/java/com/napzak/domain/store/core/entity/GenrePreferenceEntity.java
@@ -1,12 +1,17 @@
 package com.napzak.domain.store.core.entity;
 
-import jakarta.persistence.*;
+import static com.napzak.domain.store.core.entity.GenrePreferenceTableConstants.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import static com.napzak.domain.store.core.entity.GenrePreferenceTableConstants.*;
 
 @Entity
 @Table(name = TABLE_GENRE_PREFERENCE)
@@ -29,5 +34,12 @@ public class GenrePreferenceEntity {
 	private GenrePreferenceEntity(Long storeId, Long genreId) {
 		this.storeId = storeId;
 		this.genreId = genreId;
+	}
+
+	public static GenrePreferenceEntity create(Long storeId, Long genreId) {
+		return GenrePreferenceEntity.builder()
+			.storeId(storeId)
+			.genreId(genreId)
+			.build();
 	}
 }
