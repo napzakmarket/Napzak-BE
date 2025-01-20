76102c53abf2d7a96ef98b868fca49ba94c976ae
sehee <132613722+sae2say@users.noreply.github.com>
2025-01-21 06:00:14 +0900

    [refactor] #96 - entity 설정 변경 (#97)
    * [#96] refactor(productEntity): updatedAt nullable true / updatedAt default 값 제거 / productCondition true->false / title 50자 제한 / description 250자 제한
    
    * [#96] refactor(storeEntity): description 58자 제한 추가
    
    * [#96] refactor(BannerEntity): createdAt = LocalDateTime.now() 추가
    
    * [#96] refactor(ReviewEntity): updatedAt = LocalDateTime.now() 삭제 / updatedAt nullable true
    
    * [#96] fix(Store) : cover field 추가
    
    * [#96] refactor: saver에 field cover 추가
    
    * [#96] refactor: condition ture로 변경

--- src/main/java/com/napzak/domain/store/core/StoreSaver.java
+++ src/main/java/com/napzak/domain/store/core/StoreSaver.java
@@ -1,32 +1,36 @@
 package com.napzak.domain.store.core;
 
+import org.springframework.stereotype.Component;
+
 import com.napzak.domain.store.core.entity.StoreEntity;
 import com.napzak.domain.store.core.entity.enums.Role;
 import com.napzak.domain.store.core.entity.enums.SocialType;
 import com.napzak.domain.store.core.vo.Store;
+
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class StoreSaver {
-    private final StoreRepository storeRepository;
+	private final StoreRepository storeRepository;
 
-    @Transactional
-    public Store save(
-            final String nickname,
-            final String phoneNumber,
-            final Role role,
-            final String description,
-            final Long socialId,
-            final SocialType socialType,
-            final String photo
-            ){
-        final StoreEntity storeEntity = StoreEntity.create(nickname, phoneNumber, role, description, socialId, socialType, photo);
+	@Transactional
+	public Store save(
+		final String nickname,
+		final String phoneNumber,
+		final Role role,
+		final String description,
+		final Long socialId,
+		final SocialType socialType,
+		final String photo,
+		final String cover
+	) {
+		final StoreEntity storeEntity = StoreEntity.create(nickname, phoneNumber, role, description, socialId,
+			socialType, photo, cover);
 
-        storeRepository.save(storeEntity);
+		storeRepository.save(storeEntity);
 
-        return Store.fromEntity(storeEntity);
-    }
+		return Store.fromEntity(storeEntity);
+	}
 }
