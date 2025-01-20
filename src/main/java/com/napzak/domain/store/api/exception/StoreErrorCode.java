5a8a33f7dbc76e1f71cb26cfb4f48ea27d706931
sae2say <lollol0617@gmail.com>
2025-01-21 02:49:31 +0900

    [#88] feat: success code, error code 작성

--- src/main/java/com/napzak/domain/store/api/exception/StoreErrorCode.java
+++ src/main/java/com/napzak/domain/store/api/exception/StoreErrorCode.java
@@ -1,20 +1,33 @@
 package com.napzak.domain.store.api.exception;
 
+import org.springframework.http.HttpStatus;
+
 import com.napzak.global.common.exception.base.BaseErrorCode;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum StoreErrorCode implements BaseErrorCode {
-    STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 사용자가 없습니다.");
+	/*
+	404 Not Found
+	 */
+	STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 사용자가 없습니다."),
+	STORE_NOT_REGISTERED(HttpStatus.NOT_FOUND, "소셜 로그인을 먼저 진행해주세요."),
 
-    private final HttpStatus httpStatus;
-    private final String message;
+	/*
+	400 Bad Request
+	 */
+	INVALID_GENRE_PREFERENCE_COUNT(HttpStatus.BAD_REQUEST, "선호 장르 리스트는 최대 4개까지 입력할 수 있습니다."),
+	DUPLICATE_GENRE_PREFERENCES(HttpStatus.BAD_REQUEST, "선호 장르 리스트에 중복된 값이 있습니다."),
+	;
 
-    @Override
-    public HttpStatus getHttpStatus() {
-        return this.httpStatus;
-    }
+	private final HttpStatus httpStatus;
+	private final String message;
+
+	@Override
+	public HttpStatus getHttpStatus() {
+		return this.httpStatus;
+	}
 }
--- src/main/java/com/napzak/domain/store/api/exception/StoreSuccessCode.java
+++ src/main/java/com/napzak/domain/store/api/exception/StoreSuccessCode.java
@@ -1,22 +1,26 @@
 package com.napzak.domain.store.api.exception;
 
+import org.springframework.http.HttpStatus;
+
 import com.napzak.global.common.exception.base.BaseSuccessCode;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum StoreSuccessCode implements BaseSuccessCode {
-    LOGIN_SUCCESS(HttpStatus.OK, "로그인 성공"),
-    LOGOUT_SUCCESS(HttpStatus.OK, "로그아웃 성공");
+	LOGIN_SUCCESS(HttpStatus.OK, "로그인 성공"),
+	LOGOUT_SUCCESS(HttpStatus.OK, "로그아웃 성공"),
+	NORMAL_INFO_REGISTER_SUCCESS(HttpStatus.CREATED, "기본 정보 저장 성공"),
+	;
 
-    private final HttpStatus httpStatus;
-    private final String message;
+	private final HttpStatus httpStatus;
+	private final String message;
 
-    @Override
-    public HttpStatus getHttpStatus() {
-        return this.httpStatus;
-    }
+	@Override
+	public HttpStatus getHttpStatus() {
+		return this.httpStatus;
+	}
 
 }
