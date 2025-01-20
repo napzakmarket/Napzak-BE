684fff4b47c71122665f17de9f8aadab58d906f6
sae2say <lollol0617@gmail.com>
2025-01-20 20:01:13 +0900

    [#73] - refactor: @Repository 삭제

--- src/main/java/com/napzak/domain/store/core/GenrePreferenceRepository.java
+++ src/main/java/com/napzak/domain/store/core/GenrePreferenceRepository.java
@@ -4,13 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import com.napzak.domain.store.core.entity.GenrePreferenceEntity;
 
 import feign.Param;
 
-@Repository
 public interface GenrePreferenceRepository extends JpaRepository<GenrePreferenceEntity, Long> {
 
 	boolean existsByStoreId(Long storeId);
