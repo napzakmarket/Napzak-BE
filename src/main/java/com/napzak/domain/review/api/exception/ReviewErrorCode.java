7761b5ba7a85a4a9c5d761ee402826706a96d8dc
sae2say <lollol0617@gmail.com>
2025-01-20 23:16:43 +0900

    [#53] - feat: 에러코드 작성 + 사용하지 않는 dto 작성

---
+++ src/main/java/com/napzak/domain/review/api/exception/ReviewErrorCode.java
@@ -0,0 +1,23 @@
+package com.napzak.domain.review.api.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.napzak.global.common.exception.base.BaseErrorCode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ReviewErrorCode implements BaseErrorCode {
+
+	/*
+	404 Not Found
+	 */
+	REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "리뷰를 찾을 수 없습니다."),
+	;
+
+	private final HttpStatus httpStatus;
+	private final String message;
+
+}
