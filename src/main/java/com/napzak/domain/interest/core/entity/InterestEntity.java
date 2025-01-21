ed16ba297a1b28ca0264eea7a99ee8957f1d21e4
sae2say <lollol0617@gmail.com>
2025-01-21 11:47:10 +0900

    [#109] comment: 로그 확인용 코드

--- src/main/java/com/napzak/domain/interest/core/entity/InterestEntity.java
+++ src/main/java/com/napzak/domain/interest/core/entity/InterestEntity.java
@@ -1,12 +1,17 @@
 package com.napzak.domain.interest.core.entity;
 
-import jakarta.persistence.*;
+import static com.napzak.domain.interest.core.entity.InterestTableConstants.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import static com.napzak.domain.interest.core.entity.InterestTableConstants.*;
 
 @Table(name = TABLE_INTEREST)
 @Entity
