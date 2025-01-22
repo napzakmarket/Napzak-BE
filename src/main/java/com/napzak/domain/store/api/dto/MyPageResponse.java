7ff27501816ec6b4b5917cbd3db3f09c8c17296b
sehee <132613722+sae2say@users.noreply.github.com>
2025-01-23 02:15:53 +0900

    [fix] #130 - storeNickname -> storeNickName (#132)

--- src/main/java/com/napzak/domain/store/api/dto/MyPageResponse.java
+++ src/main/java/com/napzak/domain/store/api/dto/MyPageResponse.java
@@ -2,14 +2,14 @@ package com.napzak.domain.store.api.dto;
 
 public record MyPageResponse(
 	Long storeId,
-	String storeNickname,
+	String storeNickName,
 	String storePhoto
 ) {
 	public static MyPageResponse of(
 		final Long storeId,
-		final String storeNickname,
+		final String storeNickName,
 		final String storePhoto
 	) {
-		return new MyPageResponse(storeId, storeNickname, storePhoto);
+		return new MyPageResponse(storeId, storeNickName, storePhoto);
 	}
 }
