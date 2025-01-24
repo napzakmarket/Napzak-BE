df08acca47252ab04b87795a3e62ca59ea2199cb
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-01-25 06:03:48 +0900

    [refactor] #146 - 좋아요 관련 동시성 문제 해결 (#147)
    * [#146] refactor(ProductRepository): interestCount 관련 lock 추가
    
    * [#146] chore(ProductUpdater): indent 조정
    
    * [#146] refactor: lock 방식 변경

--- src/main/java/com/napzak/domain/product/core/ProductRepository.java
+++ src/main/java/com/napzak/domain/product/core/ProductRepository.java
@@ -3,15 +3,23 @@ package com.napzak.domain.product.core;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.napzak.domain.product.core.entity.ProductEntity;
 
+import jakarta.persistence.LockModeType;
+
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long>, ProductRepositoryCustom {
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@Query("SELECT p FROM ProductEntity p where p.id = :productId")
+	Optional<ProductEntity> lockById(Long productId);
+
 	@Modifying
 	@Query("""
 		UPDATE ProductEntity p
--- src/main/java/com/napzak/domain/product/core/ProductUpdater.java
+++ src/main/java/com/napzak/domain/product/core/ProductUpdater.java
@@ -3,6 +3,10 @@ package com.napzak.domain.product.core;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.napzak.domain.product.api.exception.ProductErrorCode;
+import com.napzak.domain.product.core.entity.ProductEntity;
+import com.napzak.global.common.exception.NapzakException;
+
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -13,12 +17,15 @@ public class ProductUpdater {
 
 	@Transactional
 	public void incrementInterestCount(Long productId) {
+		ProductEntity productEntity = productRepository.lockById(productId)
+			.orElseThrow(() -> new NapzakException(ProductErrorCode.PRODUCT_NOT_FOUND));
 		productRepository.incrementInterestCount(productId);
 	}
 
 	@Transactional
 	public void decrementInterestCount(Long productId) {
-
+		ProductEntity productEntity = productRepository.lockById(productId)
+			.orElseThrow(() -> new NapzakException(ProductErrorCode.PRODUCT_NOT_FOUND));
 		productRepository.decrementInterestCount(productId);
 	}
 }
