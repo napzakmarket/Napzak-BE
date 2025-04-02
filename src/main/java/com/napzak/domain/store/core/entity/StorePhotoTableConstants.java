a6934d70103a21703353b03b03de16e196bb1067
seahee <lollol0617@gmail.com>
2025-04-03 00:42:01 +0900

    [feat] #1 - storePhoto 엔티티 생성, vo 작성

---
+++ src/main/java/com/napzak/domain/store/core/entity/StorePhotoTableConstants.java
@@ -0,0 +1,8 @@
+package com.napzak.domain.store.core.entity;
+
+public class StorePhotoTableConstants {
+	public static final String TABLE_STORE_PHOTO = "store_photo";
+	public static final String COLUMN_STORE_ID = "store_id";
+	public static final String COLUMN_PHOTO_TYPE = "photo_type";
+	public static final String COLUMN_PHOTO_URL = "photo_url";
+}
