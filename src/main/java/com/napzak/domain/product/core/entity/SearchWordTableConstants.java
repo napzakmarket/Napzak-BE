47427851011685248f46347b775861570ac2c36f
seahee <lollol0617@gmail.com>
2025-04-03 02:08:57 +0900

    [feat] #1 - searchWord entity 생성, vo 작성

---
+++ src/main/java/com/napzak/domain/product/core/entity/SearchWordTableConstants.java
@@ -0,0 +1,7 @@
+package com.napzak.domain.product.core.entity;
+
+public class SearchWordTableConstants {
+	public static final String TABLE_SEARCH_WORD = "search_word";
+	public static final String COLUMN_ID = "id";
+	public static final String COLUMN_SEARCH_WORD = "search_word";
+}
