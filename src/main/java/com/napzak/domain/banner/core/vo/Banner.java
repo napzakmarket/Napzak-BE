796cdc9b7589257c3ad9e2cedfe2413a7a5c979e
seahee <lollol0617@gmail.com>
2025-04-02 23:55:33 +0900

    [feat] #1 - banner vo 추가

--- src/main/java/com/napzak/domain/banner/core/vo/Banner.java
+++ src/main/java/com/napzak/domain/banner/core/vo/Banner.java
@@ -1,36 +1,45 @@
 package com.napzak.domain.banner.core.vo;
 
 import com.napzak.domain.banner.core.entity.BannerEntity;
+import com.napzak.domain.banner.core.entity.enums.BannerType;
+
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
 public class Banner {
-    private final Long id;
-    private final String photoUrl;
-    private final int sequence;
-    private final String redirectUrl;
-    private final LocalDateTime createdAt;
-    private final LocalDateTime updatedAt;
+	private final Long id;
+	private final String photoUrl;
+	private final int sequence;
+	private final String redirectUrl;
+	private final LocalDateTime createdAt;
+	private final LocalDateTime updatedAt;
+	private final BannerType bannerType;
+	private final boolean isExternal;
 
-    public Banner(Long id, String photoUrl, int sequence, String redirectUrl, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.id = id;
-        this.photoUrl = photoUrl;
-        this.sequence = sequence;
-        this.redirectUrl = redirectUrl;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
+	public Banner(Long id, String photoUrl, int sequence, String redirectUrl, LocalDateTime createdAt,
+		LocalDateTime updatedAt, BannerType bannerType, boolean isExternal) {
+		this.id = id;
+		this.photoUrl = photoUrl;
+		this.sequence = sequence;
+		this.redirectUrl = redirectUrl;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		this.bannerType = bannerType;
+		this.isExternal = isExternal;
+	}
 
-    public static Banner fromEntity(BannerEntity bannerEntity) {
-        return new Banner(
-                bannerEntity.getId(),
-                bannerEntity.getPhotoUrl(),
-                bannerEntity.getSequence(),
-                bannerEntity.getRedirectUrl(),
-                bannerEntity.getCreatedAt(),
-                bannerEntity.getUpdatedAt()
-        );
-    }
+	public static Banner fromEntity(BannerEntity bannerEntity) {
+		return new Banner(
+			bannerEntity.getId(),
+			bannerEntity.getPhotoUrl(),
+			bannerEntity.getSequence(),
+			bannerEntity.getRedirectUrl(),
+			bannerEntity.getCreatedAt(),
+			bannerEntity.getUpdatedAt(),
+			bannerEntity.getBannerType(),
+			bannerEntity.isExternal()
+		);
+	}
 }
