8e7b17bbe1dc13f1a9b6a9c9150e971d9d774a43
seahee <lollol0617@gmail.com>
2025-04-12 22:11:39 +0900

    [feat] #6 - 배너 타입으로 특정 유형 배너 조회하도록 변경

--- src/main/java/com/napzak/domain/banner/core/BannerRetriever.java
+++ src/main/java/com/napzak/domain/banner/core/BannerRetriever.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import com.napzak.domain.banner.core.entity.BannerEntity;
+import com.napzak.domain.banner.core.entity.enums.BannerType;
 import com.napzak.domain.banner.core.vo.Banner;
 
 import lombok.RequiredArgsConstructor;
@@ -15,9 +16,9 @@ public class BannerRetriever {
 
 	private final BannerRepository bannerRepository;
 
-	public List<Banner> findAllBanners() {
+	public List<Banner> findAllBanners(BannerType bannerType) {
 
-		List<BannerEntity> bannerEntityList = bannerRepository.findAll();
+		List<BannerEntity> bannerEntityList = bannerRepository.findAllByBannerType(bannerType);
 
 		return bannerEntityList.stream()
 			.map(Banner::fromEntity)
