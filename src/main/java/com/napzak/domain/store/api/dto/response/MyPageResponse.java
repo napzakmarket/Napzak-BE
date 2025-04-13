8da2bcd1b75676b3c308a49ae3df798ac87c66ee
seahee <lollol0617@gmail.com>
2025-04-13 18:30:30 +0900

    [feat] #12 - dto에 총 팔아요,구해요 수 및 외부링크 url 추가 포함

--- src/main/java/com/napzak/domain/store/api/dto/response/MyPageResponse.java
+++ src/main/java/com/napzak/domain/store/api/dto/response/MyPageResponse.java
@@ -3,13 +3,19 @@ package com.napzak.domain.store.api.dto.response;
 public record MyPageResponse(
 	Long storeId,
 	String storeNickName,
-	String storePhoto
+	String storePhoto,
+	int totalSellCount,
+	int totalBuyCount,
+	String serviceLink
 ) {
 	public static MyPageResponse of(
 		final Long storeId,
 		final String storeNickName,
-		final String storePhoto
+		final String storePhoto,
+		final int totalSellCount,
+		final int totalBuyCount,
+		final String serviceLink
 	) {
-		return new MyPageResponse(storeId, storeNickName, storePhoto);
+		return new MyPageResponse(storeId, storeNickName, storePhoto, totalSellCount, totalBuyCount, serviceLink);
 	}
 }
