b9c48b1d0571106105197acf125d2d074cce19eb
seahee <lollol0617@gmail.com>
2025-04-25 00:32:51 +0900

    [feat] #18 - 수정을 위한 dto에도 validator가 적용되도록

---
+++ src/main/java/com/napzak/domain/product/api/dto/request/ProductPhotoBase.java
@@ -0,0 +1,5 @@
+package com.napzak.domain.product.api.dto.request;
+
+public interface ProductPhotoBase{
+	int sequence();
+}
--- src/main/java/com/napzak/domain/product/api/dto/request/ProductPhotoRequestDto.java
+++ src/main/java/com/napzak/domain/product/api/dto/request/ProductPhotoRequestDto.java
@@ -9,5 +9,9 @@ public record ProductPhotoRequestDto(
 
 	@Positive(message = "사진 순서는 1 이상이어야 합니다.")
 	int sequence
-) {
+) implements ProductPhotoBase {
+	@Override
+	public int sequence() {
+		return sequence;
+	}
 }
