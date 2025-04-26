df6edda021912ed21a11dc5fab36a0f16ee3a5eb
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-04-26 11:15:48 +0900

    [#44] feat(StorePhotoRetriever): getStorePhoto 추가

---
+++ src/main/java/com/napzak/domain/store/core/StorePhotoRetriever.java
@@ -0,0 +1,20 @@
+package com.napzak.domain.store.core;
+
+import org.springframework.stereotype.Component;
+
+import com.napzak.domain.store.core.entity.enums.PhotoType;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class StorePhotoRetriever {
+
+	private final StorePhotoRepository storePhotoRepository;
+
+	public String getStorePhoto(PhotoType photoType){
+		return storePhotoRepository.findByPhotoType(photoType)
+			.map(photo -> photo.getPhotoUrl())
+			.orElse(null);
+	}
+}
