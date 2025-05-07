e5fa3b399f49c8cbb26ae0e2bb79fce3ff032684
seahee <lollol0617@gmail.com>
2025-05-07 20:05:54 +0900

    [feat] #65 - 약관 조회 api 에서 bundle id를 함께 return

--- src/main/java/com/napzak/domain/store/api/dto/response/TermsDto.java
+++ src/main/java/com/napzak/domain/store/api/dto/response/TermsDto.java
@@ -3,13 +3,15 @@ package com.napzak.domain.store.api.dto.response;
 public record TermsDto(
 	Long termsId,
 	String termsTitle,
-	String termsUrl
+	String termsUrl,
+	int bundleId
 ) {
 	public static TermsDto from(
 		Long termsId,
 		String termsTitle,
-		String termsUrl
+		String termsUrl,
+		int bundleId
 	) {
-		return new TermsDto(termsId, termsTitle, termsUrl);
+		return new TermsDto(termsId, termsTitle, termsUrl, bundleId);
 	}
 }
