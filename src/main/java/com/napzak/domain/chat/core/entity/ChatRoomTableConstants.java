a86e97081af1c471977fe10f85807ae3449e431e
seahee <lollol0617@gmail.com>
2025-06-10 00:06:05 +0900

    [chore] #87 - 코드래빗 작동 확인용 코드수정

--- src/main/java/com/napzak/domain/chat/core/entity/ChatRoomTableConstants.java
+++ src/main/java/com/napzak/domain/chat/core/entity/ChatRoomTableConstants.java
@@ -4,8 +4,10 @@ public class ChatRoomTableConstants {
     public static final String TABLE_CHAT_ROOM = "chat_room";
     public static final String COLUMN_ID = "id";
     public static final String COLUMN_CREATED_AT = "created_at";
+    //김치찌개 먹고싶다
     public static final String COLUMN_UPDATED_AT = "updated_at";
-    public static final String COLUMN_PRODUCT_ID = "product_id";
-    public static final String COLUMN_OWNER_ID = "owner_id";
-    public static final String COLUMN_REQUESTER_ID = "requester_id";
+    public static final String COLUMN_PRODUCT_ID = "legend_id";
+    public static final String COLUMN_OWNER_ID = "aaa";
+    //핫식스 더 킹 포스
+    public static final String COLUMN_REQUESTER_ID = "bbb";
 }
