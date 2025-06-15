6cc7dfb4e1e81894416a1259921c94c82bb39cb2
seahee <lollol0617@gmail.com>
2025-06-15 18:32:55 +0900

    [feat] #89 - 찜 모아보기-팔아요 dto 작성

---
+++ src/main/java/com/napzak/domain/product/api/dto/response/InterestedSellProductResponse.java
@@ -0,0 +1,40 @@
+package com.napzak.domain.product.api.dto.response;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.napzak.domain.product.core.vo.ProductWithFirstPhoto;
+import com.napzak.global.common.util.TimeUtils;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record InterestedSellProductResponse (
+	List<ProductSellDto> interestedSellProductList,
+	@Nullable
+	String nextCursor
+) {
+	public static InterestedSellProductResponse from(
+		List<ProductWithFirstPhoto> productList,
+		Map<Long, Boolean> interestMap,
+		Map<Long, String> genreMap,
+		String nextCursor,
+		Long currentStoreId
+	) {
+		List<ProductSellDto> dtoList = productList.stream()
+			.map(product -> {
+				String uploadTime = TimeUtils.calculateUploadTime(product.getCreatedAt());
+				boolean isInterested = interestMap.getOrDefault(product.getId(), false);
+				String genreName = genreMap.getOrDefault(product.getGenreId(), "기타");
+				boolean isOwnedByCurrentUser = currentStoreId.equals(product.getStoreId());
+
+				return ProductSellDto.from(
+					product, uploadTime, isInterested, genreName, isOwnedByCurrentUser
+				);
+			})
+			.toList();
+
+		return new InterestedSellProductResponse(dtoList, nextCursor);
+	}
+}
