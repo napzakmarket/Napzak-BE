74b075f6e6abaf4f3bd4b5276e9cd7a08d8c90d4
seahee <lollol0617@gmail.com>
2025-06-15 19:04:28 +0900

    [fix] #89 - 에구, 실수..

--- src/main/java/com/napzak/domain/chat/core/entity/ChatRoomTableConstants.java
+++ src/main/java/com/napzak/domain/chat/core/entity/ChatRoomTableConstants.java
@@ -4,10 +4,8 @@ public class ChatRoomTableConstants {
     public static final String TABLE_CHAT_ROOM = "chat_room";
     public static final String COLUMN_ID = "id";
     public static final String COLUMN_CREATED_AT = "created_at";
-    //김치찌개 먹고싶다
     public static final String COLUMN_UPDATED_AT = "updated_at";
-    public static final String COLUMN_PRODUCT_ID = "legend_id";
-    public static final String COLUMN_OWNER_ID = "aaa";
-    //핫식스 더 킹 포스
-    public static final String COLUMN_REQUESTER_ID = "bbb";
+    public static final String COLUMN_PRODUCT_ID = "product_id";
+    public static final String COLUMN_OWNER_ID = "owner_id";
+    public static final String COLUMN_REQUESTER_ID = "requester_id";
 }
