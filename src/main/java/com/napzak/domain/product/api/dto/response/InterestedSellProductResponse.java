b93f3b0322ce394405d9c027d113b5cf268f2130
seahee <lollol0617@gmail.com>
2025-06-17 00:40:56 +0900

    [feat] #87 - 커서를 생성하는 데 필요한 파라미터 수정

--- src/main/java/com/napzak/domain/product/api/dto/response/InterestedSellProductResponse.java
+++ src/main/java/com/napzak/domain/product/api/dto/response/InterestedSellProductResponse.java
@@ -1,26 +1,32 @@
 package com.napzak.domain.product.api.dto.response;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Nullable;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.napzak.domain.product.api.service.enums.SortOption;
+import com.napzak.domain.product.api.service.ProductPagination;
 import com.napzak.domain.product.core.vo.ProductWithFirstPhoto;
 import com.napzak.global.common.util.TimeUtils;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record InterestedProductResponse(
+public record InterestedSellProductResponse(
 	List<ProductSellDto> interestedSellProductList,
 	@Nullable
 	String nextCursor
 ) {
-	public static InterestedProductResponse from(
+	public static InterestedSellProductResponse from(
 		List<ProductWithFirstPhoto> productList,
 		Map<Long, Boolean> interestMap,
 		Map<Long, String> genreMap,
-		String nextCursor,
-		Long currentStoreId
+		Long currentStoreId,
+		ProductPagination pagination,
+		SortOption sortOption,
+		Long lastInterestId,
+		LocalDateTime lastInterestCreatedAt
 	) {
 		List<ProductSellDto> dtoList = productList.stream()
 			.map(product -> {
@@ -35,6 +41,8 @@ public record InterestedProductResponse(
 			})
 			.toList();
 
-		return new InterestedProductResponse(dtoList, nextCursor);
+		String nextCursor = pagination.generateNextInterestCursor(sortOption, lastInterestId, lastInterestCreatedAt);
+
+		return new InterestedSellProductResponse(dtoList, nextCursor);
 	}
 }
