dbc7f4a44565003a3ae024a3002ceb41affe57e5
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-07-14 06:37:29 +0900

    [#100] feat: 채팅 api 도에인 간 facade 추가

---
+++ api-server/src/main/java/com/napzak/api/domain/product/dto/response/ProductChatInfoResponse.java
@@ -0,0 +1,43 @@
+package com.napzak.api.domain.product.dto.response;
+
+import com.napzak.common.auth.role.enums.Role;
+import com.napzak.domain.product.entity.enums.TradeType;
+import com.napzak.domain.product.vo.ProductWithFirstPhoto;
+import com.napzak.domain.store.vo.Store;
+
+public record ProductChatInfoResponse(
+	ProductInfo productInfo,
+	StoreInfo storeInfo,
+	Long roomId
+) {
+	public static ProductChatInfoResponse from(
+		ProductWithFirstPhoto product,
+		Store store,
+		String genreName,
+		Long roomId
+	) {
+		boolean isWithdrawn = store.getRole().equals(Role.WITHDRAWN);
+		String nickname = isWithdrawn ? "(탈퇴한 사용자) " + store.getNickname() : store.getNickname();
+		return new ProductChatInfoResponse(
+			new ProductInfo(
+				product.getId(),
+				product.getFirstPhoto(),
+				product.getTradeType(),
+				product.getTitle(),
+				product.getPrice(),
+				product.getIsPriceNegotiable(),
+				genreName
+			),
+			new StoreInfo(
+				store.getId(),
+				nickname,
+				store.getPhoto(),
+				isWithdrawn
+			),
+			roomId
+		);
+	}
+
+	public record ProductInfo(Long productId, String photo, TradeType tradeType, String title, int price, Boolean isPriceNegotiable, String genreName) {}
+	public record StoreInfo(Long storeId, String nickname, String storePhoto, boolean isWithdrawn) {}
+}
