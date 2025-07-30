9b0e425953b41953073997c87f3d8d45483e4c02
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-07-30 18:56:56 +0900

    [#137] fix(RoomDateKey): 날짜 메시지 중복 방지를 위한 RoomDateKey 도입

---
+++ core-domain/src/main/java/com/napzak/domain/chat/vo/RoomDateKey.java
@@ -0,0 +1,30 @@
+package com.napzak.domain.chat.vo;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import lombok.Getter;
+
+@Getter
+public class RoomDateKey {
+	private final Long roomId;
+	private final LocalDate date;
+
+	public RoomDateKey(Long roomId, LocalDate date) {
+		this.roomId = roomId;
+		this.date = date;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof RoomDateKey)) return false;
+		RoomDateKey that = (RoomDateKey) o;
+		return Objects.equals(roomId, that.roomId) && Objects.equals(date, that.date);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(roomId, date);
+	}
+}
