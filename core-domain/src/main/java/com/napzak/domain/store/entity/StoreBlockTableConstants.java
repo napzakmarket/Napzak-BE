b9b9f2177ec1703a1b0b03f0d92bdd42a211f443
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-09-20 00:32:47 +0900

    [#208] feat: 차단 entity 생성

---
+++ core-domain/src/main/java/com/napzak/domain/store/entity/StoreBlockTableConstants.java
@@ -0,0 +1,8 @@
+package com.napzak.domain.store.entity;
+
+public class StoreBlockTableConstants {
+	public static final String TABLE_STORE_BLOCK = "store_block";
+	public static final String COLUMN_ID = "id";
+	public static final String COLUMN_BLOCKER_STORE_ID = "blocker_store_id";
+	public static final String COLUMN_BLOCKED_STORE_ID = "blocked_store_id";
+}
