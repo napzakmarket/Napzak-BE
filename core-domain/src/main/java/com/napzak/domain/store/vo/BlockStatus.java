53f59cf2ff21f425cc5ad0d1c3e5b829876bff4a
hyerinhwang-sailin <sallyh1111@ewhain.net>
2025-09-21 23:21:21 +0900

    [#211] chore(BlockStatus): 설명 주석 추가

--- core-domain/src/main/java/com/napzak/domain/store/vo/BlockStatus.java
+++ core-domain/src/main/java/com/napzak/domain/store/vo/BlockStatus.java
@@ -1,7 +1,7 @@
 package com.napzak.domain.store.vo;
 
 public record BlockStatus(
-	boolean isOpponentStoreBlocked,
-	boolean isChatBlocked
+	boolean isOpponentStoreBlocked, // 상대방 상점을 차단했는지 여부
+	boolean isChatBlocked // 상대방이 내 상점을 차단했는지 여부
 ) {
 }
