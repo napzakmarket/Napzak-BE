6ed8b93383ad27dd591ffe97dddfc13cd5e66a5c
seahee <lollol0617@gmail.com>
2026-04-20 13:31:48 +0900

    [feat] #262 - 블랙리스트 엔티티 생성

---
+++ core-domain/src/main/java/com/napzak/domain/store/entity/BlacklistTableConstants.java
@@ -0,0 +1,9 @@
+package com.napzak.domain.store.entity;
+
+public class BlacklistTableConstants {
+	public static final String TABLE_BLACKLIST = "blacklist";
+	public static final String COLUMN_ID = "id";
+	public static final String COLUMN_STORE_ID = "store_id";
+	public static final String COLUMN_PHONE_NUMBER_ENC = "phone_number_enc";
+	public static final String COLUMN_PHONE_NUMBER_HASH = "phone_number_hash";
+}
