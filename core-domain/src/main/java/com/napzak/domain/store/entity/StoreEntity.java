71cea1de60b16deec4eaa69157a200f9a44db87e
seahee <lollol0617@gmail.com>
2026-05-14 23:15:46 +0900

    [refactor] #276 - 전화번호 hash 필드에 유니크 제약 삭제, 사용하지 않는 메서드 삭제

--- core-domain/src/main/java/com/napzak/domain/store/entity/StoreEntity.java
+++ core-domain/src/main/java/com/napzak/domain/store/entity/StoreEntity.java
@@ -12,19 +12,13 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Table(
-	name = StoreTableConstants.TABLE_STORE,
-	indexes = {
-		@Index(name = "uk_store_phone_number_hash", columnList = StoreTableConstants.COLUMN_PHONE_NUMBER_HASH, unique = true)
-	}
-)
+@Table(name = StoreTableConstants.TABLE_STORE)
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -164,13 +158,6 @@ public class StoreEntity {
 	}
 
 	public void markPhoneVerified() {
-		this.phoneVerified = true;
-		this.verifiedAt = LocalDateTime.now();
-	}
-
-	public void verifyAndUpdatePhone(String phoneNumberEnc, String phoneNumberHash) {
-		this.phoneNumberEnc = phoneNumberEnc;
-		this.phoneNumberHash = phoneNumberHash;
 		this.phoneVerified = true;
 		this.verifiedAt = LocalDateTime.now();
 	}
